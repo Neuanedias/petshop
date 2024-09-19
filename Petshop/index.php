23bb07bd3a84714ff0ebca7160067e5763dc698e
Neuanedias <161649937+Neuanedias@users.noreply.github.com>
2024-09-19 11:44:03 -0400

    Add files via upload

---
+++ Petshop/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>petshop</title>
+</head>
+<body>
+    
+<h1>Cadastro do colaborador</h1> 
+<h2>Nome colaborador</h2>
+<h2>CPF colaborador</h2>
+
+<button>cadastrar</button>
+
+</body>
+</html>
+
---
+++ prova.1.sql
@@ -0,0 +1,23 @@
+create database petshop;
+use petshop;
+
+create table colaborador(
+primary key id,
+nome varchar (50),
+cpf varchar (50)
+);
+
+create table atividades(
+primary key id,
+prioridade varchar(50)
+solicitacao e situacao varchar(50)
+titulo varchar (50),
+descricao varchar (50)
+);
+
+
+
+
+
+
+
